bececee4d363d54a232ad131a546f28674644d69
gavinkong <a1458661@gmai.com>
2022-12-11 02:43:20 +0800

    remove aside padding

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,7 +12,7 @@ export default function MyApp({ Component, pageProps: { session, ...pageProps } 
           <title>Spotify Profile</title>
           <link rel="shortcut icon" href="/spotify.svg" />
         </Head>
-        <div className='flex flex-col-reverse md:flex-row'>
+        <div className='flex flex-col-reverse md:flex-row h-[-webkit-fill-available]'>
           <Aside />
           <Component {...pageProps} />
         </div>
